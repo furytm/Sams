658448da0cae171398a2c8e6e9e3b4888d5e43b8
furytm <kanuemma367@gmail.com>
2025-05-14 11:18:11 +0100

    added sams base url

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,26 @@ import subscriberRouter from './routes/subscribe';
 import authRouter from './routes/auth.routes';
 import { scheduleUserCleanup } from './jobs/deleteExpiredUsers.job';
 import tokenRouter from './routes/token.routes'
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 app.use(express.json())
 app.use(bodyParser.json());
+app.use(cookieParser()); 
 const port = 3000;
 
-app.use(
-    cors({
-      origin: "*", // Allows all origins
-      methods: 'GET, POST, PUT, DELETE, OPTIONS',
-      allowedHeaders: 'Content-Type, Authorization',
-      credentials: true, // Allow cookies and authorization headers
-    })
-  );
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://school-managemnet.netlify.app',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
   
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
