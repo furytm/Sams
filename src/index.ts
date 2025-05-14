82f88beac8a468694f1a5d63911ab3822cd4276e
furytm <kanuemma367@gmail.com>
2025-05-14 11:56:43 +0100

    added front edn local host

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const port = 3000;
 
 app.use(cors({
   origin: [
-    'http://localhost:3000',
+    'http://localhost:3000','http://localhost:5173','http://localhost:5174',
     'https://school-managemnet.netlify.app',
   ],
   credentials: true,
